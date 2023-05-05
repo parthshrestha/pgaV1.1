1a75632b0683b189782c36133fb3d0e10c497004
parthshrestha <77508075+parthshrestha@users.noreply.github.com>
2023-05-05 14:08:19 -0600

    update 0.2

--- Activity/Selling.java
+++ Activity/Selling.java
@@ -2,6 +2,8 @@ package Activity;
 
 import Observer.Publisher;
 import buyer.Customer;
+import decorator.ClubCover;
+import decorator.Warranty;
 import goods.Item;
 import staff.SoftGood;
 import staff.Staff;
@@ -16,7 +18,45 @@ public class Selling implements Publisher {
     {
         pgaStorenumber = store;
     }
-    public double selling(Customer customer, ArrayList<Staff>[] employees, ArrayList<Item> inventory, ArrayList<Item> soldInventory, double netSales, double staffEarnigns)
+    public double sellAddons(Item i)//helper function to instantiate decorator pattern
+    {
+        Item temp = i;
+        Random random = new Random();
+        double soldAddon = 0;
+        double chanceWarranty = random.nextDouble();
+        double chanceHeadcover = random.nextDouble();
+
+        if(chanceWarranty <= 0.3)// 25% chance  buyer buys it
+        {
+            temp = new Warranty(i);
+            i.setPrice(temp.getPrice());
+            soldAddon += temp.getInitialPrice() *0.3;//price of add-on
+            i.setModel(temp.getModel());
+            System.out.print(i.getBrand()+" "+i.getModel()+" "+i.getType());
+
+            System.out.println( "CLub after Extended warranty costs:" + i.getPrice());
+            notifySubscriber("log",i.getBrand()+" "+i.getModel()+" "+i.getType());
+            notifySubscriber("log","CLub after Extended warranty costs:" + i.getPrice());
+
+        }
+        if(chanceHeadcover<=0.4)//40% chance
+        {
+            temp = new ClubCover(i);
+            i.setPrice(temp.getPrice());
+            soldAddon += temp.getInitialPrice() *0.3;//price of add-on
+            i.setModel(temp.getModel());
+            System.out.print(i.getBrand()+" "+i.getModel()+" "+i.getType());
+
+            System.out.println( "CLub after Extra Head Cover costs:" + i.getPrice());
+            notifySubscriber("log",i.getBrand()+" "+i.getModel()+" "+i.getType());
+            notifySubscriber("log","CLub after Extra Head Cover costs:" + i.getPrice());
+
+        }
+
+
+        return soldAddon;
+    }
+    public double selling(Customer customer, ArrayList<Staff>[] employees, ArrayList<Item> inventory, ArrayList<Item> soldInventory, double netSales, double staffEarnigns, ArrayList pastCustomers)
     {
         System.out.println("Starting selling...");
         Random rand = new Random();
@@ -32,11 +72,13 @@ public class Selling implements Publisher {
                 customer.addToCart(currItem);
                 System.out.println("Customer: "+ customer.name +" choose to buy "+ currItem.getBrand() + " "+currItem.getModel()+" :" + currItem.getPrice());
                 netSales += currItem.getPrice();
+                netSales -= sellAddons(currItem);
                 double bonus = currItem.getPrice() * 0.1;
                 System.out.println("Associate: "+ currEmployee.getName()+ "recived bonus- "+ bonus);
                 netSales-= bonus;
                 currEmployee.setBonus(bonus);
                 notifySubscriber("staff",String.valueOf(bonus));
+
                 soldInventory.add(currItem);//add to sold inventory
                 inventory.remove(currItem);//remove from inventory
                 //recountinventory
@@ -49,7 +91,9 @@ public class Selling implements Publisher {
             }
             //show receipt
         }
+        notifySubscriber("budgetAdd",String.valueOf(netSales));
         System.out.println("done selling");
+        pastCustomers.add(customer);
         return netSales;
     }
 }
